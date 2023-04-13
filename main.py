93a6ad9e37ce455ecc28d8b8673d70b83f7173f1
killopro1234 <98070069+killopro1234@users.noreply.github.com>
2023-04-13 17:54:35 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from tkinter Import *
+from PIL import ImageTK,Image
+from tkinter import ttk
+root=TK()
+
+Listed_items : ['Bottle','Tiffin','ID Card','Chocolates','Chips','Tickets','Hanky']
+Put Item no [1] in bag
+
+label1["text"] = "Listed_items : " + str(List1)
+random.sample(range(0,3),1)
+label2["text"] = "Put Item no " + str(random_List)+ ""
